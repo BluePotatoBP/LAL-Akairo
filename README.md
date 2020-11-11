e8649e3da59921504c569284109ca9fdd8931d5a
BluePotatoBP <lprwasunavailable@gmail.com>
2020-11-11 22:03:46 +0100

    added a propper README.md and updated translations

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# LAL-Akairo
+<h1 align="center">Welcome to likealight 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.2.5-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/BluePotatoBP" target="_blank">
+    <img alt="Twitter: BluePotatoBP" src="https://img.shields.io/twitter/follow/BluePotatoBP.svg?style=social" />
+  </a>
+</p>
+
+> Discord bot made in akairo
+
+## Usage
+
+```sh
+(npm run dev)
+```
+
+## Author
+
+👤 **BluePotatoBP**
+
+* Twitter: [@BluePotatoBP](https://twitter.com/BluePotatoBP)
+* Github: [@bluepotatobp](https://github.com/bluepotatobp)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- commands/Util/invite.js
+++ commands/Util/invite.js
@@ -20,7 +20,7 @@ class Invite extends Command {
 
         let iembed = new Discord.MessageEmbed()
             .setAuthor(message.author.username, message.author.avatarURL({ dynamic: true }))
-            .setDescription(`[${lang(message, "command.invite.iembed.desc.one")}](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8) ${lang(message, "command.invite.iembed.desc.two")} [${lang(message, "command.invite.iembed.desc.one")}](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2080759927) ${lang(message, "command.invite.iembed.desc.three")} [${lang(message, "command.invite.iembed.desc.one")}](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2048) ${lang(message, "command.invite.iembed.desc.four")} <a:blobEat:773207674015055912>`)
+            .setDescription(`[${lang(message, "command.invite.iembed.desc.one")}](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8) ${lang(message, "command.invite.iembed.desc.two")} [${lang(message, "command.invite.iembed.desc.one")}](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=470154327) ${lang(message, "command.invite.iembed.desc.three")} [${lang(message, "command.invite.iembed.desc.one")}](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2048) ${lang(message, "command.invite.iembed.desc.four")} <a:blobEat:773207674015055912>`)
             .setColor(crimson)
             .setFooter(`🎉 Copyright © BluePotatoBP - 2020 🎉`, `${client.user.avatarURL({ dynamic: true })}`)
             .setTimestamp()
