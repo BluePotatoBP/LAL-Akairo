6d87f4a72c5e3bb1875b3924e19b9dc87c3e051c
BluePotatoBP <lprwasunavailable@gmail.com>
2020-12-15 15:28:39 +0100

    fixed the google command + added a cutTo function

--- assets/tools/util.js
+++ assets/tools/util.js
@@ -1,43 +1,58 @@
 module.exports = {
-    getMember: function(message, toFind = '') {
-        toFind = toFind.toLowerCase();
+	getMember: function(message, toFind = '') {
+		toFind = toFind.toLowerCase();
 
-        let target = message.guild.members.cache.get(toFind);
-        
-        if (!target && message.mentions.members)
-            target = message.mentions.members.first();
+		let target = message.guild.members.cache.get(toFind);
 
-        if (!target && toFind) {
-            target = message.guild.members.find(member => {
-                return member.displayName.toLowerCase().includes(toFind) ||
-                member.user.tag.toLowerCase().includes(toFind)
-            });
-        }
-            
-        if (!target) 
-            target = message.member;
-            
-        return target;
-    },
+		if (!target && message.mentions.members) target = message.mentions.members.first();
 
-    formatDate: function(date) {
-        return new Intl.DateTimeFormat('en-US').format(date)
-    },
+		if (!target && toFind) {
+			target = message.guild.members.find((member) => {
+				return (
+					member.displayName.toLowerCase().includes(toFind) || member.user.tag.toLowerCase().includes(toFind)
+				);
+			});
+		}
 
-    promptMessage: async function (message, author, time, validReactions) {
-        // We put in the time as seconds, with this it's being transfered to MS
-        time *= 1000;
+		if (!target) target = message.member;
 
-        // For every emoji in the function parameters, react in the good order.
-        for (const reaction of validReactions) await message.react(reaction);
+		return target;
+	},
 
-        // Only allow reactions from the author, 
-        // and the emoji must be in the array we provided.
-        const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
+	cutTo: function(input, from = 0, to = 250, ending = true) {
+		/* NOTE: Does not check for ' '(spaces) */
+		if (input.length > to) {
+			let output = input.substring(from, to);
 
-        // And ofcourse, await the reactions
-        return message
-            .awaitReactions(filter, { max: 1, time: time})
-            .then(collected => collected.first() && collected.first().emoji.name);
-    }
+			if (ending) {
+				return output + '...';
+			} else {
+				return output;
+			}
+		} else {
+			input = s;
+			return input;
+		}
+	},
+
+	formatDate: function(date) {
+		return new Intl.DateTimeFormat('en-US').format(date);
+	},
+
+	promptMessage: async function(message, author, time, validReactions) {
+		// We put in the time as seconds, with this it's being transfered to MS
+		time *= 1000;
+
+		// For every emoji in the function parameters, react in the good order.
+		for (const reaction of validReactions) await message.react(reaction);
+
+		// Only allow reactions from the author,
+		// and the emoji must be in the array we provided.
+		const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
+
+		// And ofcourse, await the reactions
+		return message
+			.awaitReactions(filter, { max: 1, time: time })
+			.then((collected) => collected.first() && collected.first().emoji.name);
+	}
 };
--- commands/Fun/google.js
+++ commands/Fun/google.js
@@ -1,58 +1,43 @@
 const { Command } = require('discord-akairo');
 const Discord = require('discord.js');
-const { crimson } = require('../../assets/colors.json')
+const { crimson } = require('../../assets/colors.json');
+const { cutTo } = require('../../assets/tools/util');
 
 class Google extends Command {
-    constructor() {
-        super('google',
-            {
-                aliases: ['google', 'whatis', 'g'],
-                category: 'Fun',
-                ownerOnly: false,
-                cooldown: 10000,
-                description: {
-                    content: 'For the people that can\'t google themselves',
-                    usage: '<query>',
-                    syntax: '<> - necessary'
-                },
-                args: [
-                    {
-                        id: 's',
-                        match: 'text',
-                        type: 'string',
-                        prompt: {
-                            start: message => lang(message, "command.google.prompt.start"),
-                            retry: message => lang(message, "command.google.prompt.retry"),
-                        }
-                    },
-                ]
-            });
-    }
+	constructor() {
+		super('google', {
+			aliases: [ 'google', 'whatis' ],
+			category: 'Fun',
+			ownerOnly: false,
+			cooldown: 10000,
+			description: {
+				content: "For the people that can't google themselves",
+				usage: '<query>',
+				syntax: '<> - necessary'
+			},
+			args: [
+				{
+					id: 's',
+					match: 'text',
+					type: 'string',
+					prompt: {
+						start: (message) => lang(message, 'command.google.prompt.start'),
+						retry: (message) => lang(message, 'command.google.prompt.retry')
+					}
+				}
+			]
+		});
+	}
 
-    async exec(message, { s }) {
-        message.delete().catch(e => { });
-
-        function cut(text) {
-            if (s.length > 250) {
-                let res = s.length - 250;
-                text = text.slice(-res)
-
-                return text;
-            } else {
-                text = s;
-                return text;
-            }
-        };
-
-        let query = s.split(" ").join("+");
-        let embed = new Discord.MessageEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL({ dynamic: true }))
-            .setTitle(`"${cut(s) + '...'}"`)
-            .setDescription(`[${lang(message, "command.google.embed.desc")}](http://lmgtfy.com/?iie=1&q=${query})`)
-            .setColor('RANDOM');
-
-        message.channel.send(embed)
-
-    }
+	async exec(message, { s }) {
+		message.delete().catch((e) => {});
+		let query = s.split(' ').join('+');
+		let embed = new Discord.MessageEmbed()
+			.setAuthor(message.author.username, message.author.avatarURL({ dynamic: true }))
+			.setTitle(`"${cutTo(s)}"`)
+			.setDescription(`[${lang(message, 'command.google.embed.desc')}](http://lmgtfy.com/?iie=1&q=${query})`)
+			.setColor('RANDOM');
+		message.channel.send(embed);
+	}
 }
 module.exports = Google;
