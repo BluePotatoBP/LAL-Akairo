79795a899c1e01f7f05e28a52d86c3866c56d166
BluePotatoBP <lprwasunavailable@gmail.com>
2021-08-24 19:11:09 +0200

    code cleanup

--- listeners/antiAdvertisement.js
+++ listeners/antiAdvertisement.js
@@ -14,29 +14,17 @@ module.exports = class antiAdvertisement extends Listener {
         let regex;
 
         if (!cachedGuild) return; // If theres no data for the guild, return
-
-        if (!cachedGuild.preset) { cachedGuild.preset = 'moderate' }; // If theres no preset set return default preset
-
+        if (!cachedGuild.preset) cachedGuild.preset = 'moderate'; // If theres no preset set return default preset
         // Presets
-        if (cachedGuild.preset === 'light') {
-            regex = ('http(s)[:]\/\/|www[.]|sftp[:]|ftp[:]|[.]com|[.]net|[.]org');
-        }
-        if (cachedGuild.preset === 'moderate') {
-            regex = ('http(s)[:]\/\/|www[.]|sftp[:]|ftp[:]|[.]com|[.]net|[.]gg|[.]edu|[.]gov|[.]org|[.]moe|[.]academy|[.]tech|[.]biz|[.].aero');
-        }
-        if (cachedGuild.preset === 'heavy') {
-            regex = ('http(s)[:]\/\/|www[.]|sftp[:]|ftp[:]|[.]aero|[.]academy|[.]tech|[.]moe|[.]biz|[.]cat|[.]com|[.]coop|[.]edu|[.]gov|[.]info|[.]int|[.]jobs|[.]mil|[.]mobi|[.]museum[.]name|[.]net|[.]org|[.]travel|[.]ac|[.]ad|[.]ae|[.]af|[.]ag|[.]ai|[.]al|[.]am|[.]an|[.]ao|[.]aq|[.]ar|[.]as|[.]at|[.]au|[.]aw[.]az|[.]ba|[.]bb|[.]bd|[.]be|[.]bf|[.]bg|[.]bh|[.]bi|[.]bj|[.]bm|[.]bn|[.]bo|[.]br|[.]bs|[.]bt|[.]bv|[.]bw|[.]by|[.]bz|[.]ca[.]cc|[.]cd|[.]cf|[.]cg|[.]ch|[.]ci|[.]ck|[.]cl|[.]cm|[.]cn|[.]co|[.]cr|[.]cs|[.]cu|[.]cv|[.]cx|[.]cy|[.]cz|[.]de|[.]dj|[.]dk|[.]dm[.]do|[.]dz|[.]ec|[.]ee|[.]eg|[.]eh|[.]er|[.]es|[.]et|[.]eu|[.]fi|[.]fj|[.]fk|[.]fm|[.]fo|[.]fr|[.]ga|[.]gb|[.]gd|[.]ge|[.]gf|[.]gg|[.]gh[.]gi|[.]gl|[.]gm|[.]gn|[.]gp|[.]gq|[.]gr|[.]gs|[.]gt|[.]gu|[.]gw|[.]gy|[.]hk|[.]hm|[.]hn|[.]hr|[.]ht|[.]hu|[.]id|[.]ie|[.]il|[.]im[.]in|[.]io|[.]iq|[.]ir|[.]is|[.]it|[.]je|[.]jm|[.]jo|[.]jp|[.]ke|[.]kg|[.]kh|[.]ki|[.]km|[.]kn|[.]kp|[.]kr|[.]kw|[.]ky|[.]kz|[.]la|[.]lb[.]lc|[.]li|[.]lk|[.]lr|[.]ls|[.]lt|[.]lu|[.]lv|[.]ly|[.]ma|[.]mc|[.]md|[.]mg|[.]mh|[.]mk|[.]ml|[.]mm|[.]mn|[.]mo|[.]mp|[.]mq[.]mr|[.]ms|[.]mt|[.]mu|[.]mv|[.]mw|[.]mx|[.]my|[.]mz|[.]na|[.]nc|[.]ne|[.]nf|[.]ng|[.]ni|[.]nl|[.]no|[.]np|[.]nr|[.]nu[.]nz|[.]om|[.]pa|[.]pe|[.]pf|[.]pg|[.]ph|[.]pk|[.]pl|[.]pm|[.]pn|[.]pr|[.]ps|[.]pt|[.]pw|[.]py|[.]qa|[.]re|[.]ro|[.]ru|[.]rw[.]sa|[.]sb|[.]sc|[.]sd|[.]se|[.]sg|[.]sh|[.]si|[.]sj|[.]sk|[.]sl|[.]sm|[.]sn|[.]so|[.]sr|[.]st|[.]sv|[.]sy|[.]sz|[.]tc|[.]td|[.]tf[.]tg|[.]th|[.]tj|[.]tk|[.]tm|[.]tn|[.]to|[.]tp|[.]tr|[.]tt|[.]tv|[.]tw|[.]tz|[.]ua|[.]ug|[.]uk|[.]um|[.]us|[.]uy|[.]uz|[.]va|[.]vc[.]ve|[.]vg|[.]vi|[.]vn|[.]vu|[.]wf|[.]ws|[.]ye|[.]yt|[.]yu|[.]za|[.]zm|[.]zr|[.]zw')
-        }
+        if (cachedGuild.preset === 'light') regex = ('http(s)[:]\/\/|www[.]|sftp[:]|ftp[:]|[.]com|[.]net|[.]org');
+        if (cachedGuild.preset === 'moderate') regex = ('http(s)[:]\/\/|www[.]|sftp[:]|ftp[:]|[.]com|[.]net|[.]gg|[.]edu|[.]gov|[.]org|[.]moe|[.]academy|[.]tech|[.]biz|[.].aero');
+        if (cachedGuild.preset === 'heavy') regex = ('http(s)[:]\/\/|www[.]|sftp[:]|ftp[:]|[.]aero|[.]academy|[.]tech|[.]moe|[.]biz|[.]cat|[.]com|[.]coop|[.]edu|[.]gov|[.]info|[.]int|[.]jobs|[.]mil|[.]mobi|[.]museum[.]name|[.]net|[.]org|[.]travel|[.]ac|[.]ad|[.]ae|[.]af|[.]ag|[.]ai|[.]al|[.]am|[.]an|[.]ao|[.]aq|[.]ar|[.]as|[.]at|[.]au|[.]aw[.]az|[.]ba|[.]bb|[.]bd|[.]be|[.]bf|[.]bg|[.]bh|[.]bi|[.]bj|[.]bm|[.]bn|[.]bo|[.]br|[.]bs|[.]bt|[.]bv|[.]bw|[.]by|[.]bz|[.]ca[.]cc|[.]cd|[.]cf|[.]cg|[.]ch|[.]ci|[.]ck|[.]cl|[.]cm|[.]cn|[.]co|[.]cr|[.]cs|[.]cu|[.]cv|[.]cx|[.]cy|[.]cz|[.]de|[.]dj|[.]dk|[.]dm[.]do|[.]dz|[.]ec|[.]ee|[.]eg|[.]eh|[.]er|[.]es|[.]et|[.]eu|[.]fi|[.]fj|[.]fk|[.]fm|[.]fo|[.]fr|[.]ga|[.]gb|[.]gd|[.]ge|[.]gf|[.]gg|[.]gh[.]gi|[.]gl|[.]gm|[.]gn|[.]gp|[.]gq|[.]gr|[.]gs|[.]gt|[.]gu|[.]gw|[.]gy|[.]hk|[.]hm|[.]hn|[.]hr|[.]ht|[.]hu|[.]id|[.]ie|[.]il|[.]im[.]in|[.]io|[.]iq|[.]ir|[.]is|[.]it|[.]je|[.]jm|[.]jo|[.]jp|[.]ke|[.]kg|[.]kh|[.]ki|[.]km|[.]kn|[.]kp|[.]kr|[.]kw|[.]ky|[.]kz|[.]la|[.]lb[.]lc|[.]li|[.]lk|[.]lr|[.]ls|[.]lt|[.]lu|[.]lv|[.]ly|[.]ma|[.]mc|[.]md|[.]mg|[.]mh|[.]mk|[.]ml|[.]mm|[.]mn|[.]mo|[.]mp|[.]mq[.]mr|[.]ms|[.]mt|[.]mu|[.]mv|[.]mw|[.]mx|[.]my|[.]mz|[.]na|[.]nc|[.]ne|[.]nf|[.]ng|[.]ni|[.]nl|[.]no|[.]np|[.]nr|[.]nu[.]nz|[.]om|[.]pa|[.]pe|[.]pf|[.]pg|[.]ph|[.]pk|[.]pl|[.]pm|[.]pn|[.]pr|[.]ps|[.]pt|[.]pw|[.]py|[.]qa|[.]re|[.]ro|[.]ru|[.]rw[.]sa|[.]sb|[.]sc|[.]sd|[.]se|[.]sg|[.]sh|[.]si|[.]sj|[.]sk|[.]sl|[.]sm|[.]sn|[.]so|[.]sr|[.]st|[.]sv|[.]sy|[.]sz|[.]tc|[.]td|[.]tf[.]tg|[.]th|[.]tj|[.]tk|[.]tm|[.]tn|[.]to|[.]tp|[.]tr|[.]tt|[.]tv|[.]tw|[.]tz|[.]ua|[.]ug|[.]uk|[.]um|[.]us|[.]uy|[.]uz|[.]va|[.]vc[.]ve|[.]vg|[.]vi|[.]vn|[.]vu|[.]wf|[.]ws|[.]ye|[.]yt|[.]yu|[.]za|[.]zm|[.]zr|[.]zw')
 
         // Delete function, check if msg matches regex
         async function del() {
-            let match = await message.content.match(regex)
-
-            if (match) {
+            if (await message.content.match(regex)) {
                 await message.delete()
-                if (cachedGuild.warn === 'true') {
-                    await message.channel.send({ content: `${message.author}, links are not allowed.` }).then(e => e.delete({ timeout: 5000 }));
-                }
+                if (cachedGuild.warn === 'true') await message.channel.send({ content: `${message.author}, links are not allowed.` }).then(e => e.delete({ timeout: 5000 }));
             }
         }
 
@@ -52,9 +40,7 @@ module.exports = class antiAdvertisement extends Listener {
 
                 let memberRoles = message.member._roles
                 if (!memberRoles.some(r => role.id === r)) return del() // User isnt staff, delete message
-            } else {
-                return del()
-            }
+            } else return del()
         }
     };
 }
