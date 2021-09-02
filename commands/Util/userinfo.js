46f52c2f9ecf0a028f1d034b66d41d5b4f5e4270
BluePotatoBP <lprwasunavailable@gmail.com>
2021-09-02 19:14:26 +0200

    fixed userinfo for v13 :)))

--- commands/Util/userinfo.js
+++ commands/Util/userinfo.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
 const { crimson } = require('../../assets/colors.json');
-const { delMsg } = require('../../assets/tools/util');
+const { delMsg, cutTo } = require('../../assets/tools/util');
 
 class Userinfo extends Command {
     constructor() {
@@ -12,7 +12,7 @@ class Userinfo extends Command {
             ratelimit: 2,
             ownerOnly: false,
             description: {
-                content: 'Shows useful information about any member',
+                content: 'Shows useful information about any user',
                 usage: '<user>',
                 syntax: '<> - necessary'
             },
@@ -27,102 +27,91 @@ class Userinfo extends Command {
     async exec(message, { u }) {
         await delMsg(message);
 
-        let question = u.presence.status;
-        let capitalize = question.charAt(0).toUpperCase() + question.slice(1);
+        // Predefine some vars for later use
         let m = message.guild.members.cache.get(u.id);
+        let highestRole = m.roles.highest.name;
+        let statusState;
+        let usergame;
+        let statusEmojis = {
+            dnd: '<:dnd:773212850364743742>',
+            online: '<:online:773212850733711360>',
+            idle: '<:idle:773212850533171211>',
+            offline: '<:offline:773212850755862538>',
+            streaming: '<:streaming:773212851174506565>'
+        }
+        // Check if user is on mobile and then show icon if they are
+        let statusIndicator;
+        if (m.presence) {
+            if (m.presence.clientStatus.mobile === null) {
+                statusIndicator = `${statusEmojis[m.presence.status]} <:mobile:802732331571740742>`
+            } else statusIndicator = `${statusEmojis[m.presence.status]}`
+        } else statusIndicator = statusEmojis.offline
+
+        // If the given user isnt a member in that guild, send alternate embed
         if (!m) {
-            let status = {
-                dnd: '\`Do Not Disturb\` <:dnd:773212850364743742>',
-                online: '\`Online\` <:online:773212850733711360>',
-                idle: '\`Idle\` <:idle:773212850533171211>',
-                offline: '\`Offline\` <:offline:773212850755862538>',
-                streaming: '\`Streaming\` <:streaming:773212851174506565>'
-            }
-            const alternateEmbed = this.client.util.embed()
+            const alternateEmbed = new MessageEmbed()
                 .setAuthor(u.tag, u.displayAvatarURL({ dynamic: true }))
                 .setDescription(`${u}\n \n**Info:** This user is not in your current guild.`)
                 .addField(`Registered On:`, `\`${this.client.users.cache.get(u.id).createdAt.toUTCString().substr(0, 16)}\` <a:animatedCool:773205297782325259>`, true)
-                .addField(`Status:`, `${status[u.presence.status]}`, true)
                 .addField(`Avatar:`, `[PNG](${u.displayAvatarURL({ format: 'png', size: 4096 })} 'Link to the PNG Avatar')|[JPG](${u.displayAvatarURL({ format: 'jpg', size: 4096 })} 'Link to the PNG Avatar')|[GIF](${u.displayAvatarURL({ format: 'gif', size: 4096 })} 'Link to the GIF Avatar')`, true)
                 .setColor(crimson)
                 .setFooter(`ID: ${u.id}`)
                 .setTimestamp()
             return message.channel.send({ embeds: [alternateEmbed] })
-        }
-        let highestRole = m.roles.highest.name;
-
-        let usergame;
-        let statusState;
-        for (let i = u.presence.activities.length - 1; i >= 0; i--) {
-            if (u.presence.activities[i].type == 'CUSTOM_STATUS') {
-                statusState = 'Custom Status:'
-                usergame = `\`${u.presence.activities[i].state}\``;
-            } else if (u.presence.activities[i].type == 'PLAYING') {
-                statusState = 'Playing:'
-                usergame = u.presence.activities[i].name;
-                if (usergame.length > 20) {
-                    usergame = `\`${usergame.substr(0, 20)}...\``;
-                } else {
-                    usergame = `\`${u.presence.activities[i].name}\``;
-                }
-            } else if (u.presence.activities[i].type == 'LISTENING') {
-                statusState = 'Listening to:'
-                let songName = u.presence.activities[i].details;
-                if (songName.length > 15) {
-                    songName = songName.substr(0, 15) + '...'
-                }
-                let artist = u.presence.activities[i].state;
-                if (artist.length > 20) {
-                    artist = artist.substr(0, 20)
-                }
-                let game = u.presence.activities[i].name.substr(0, 7)
-                usergame = `\`${songName}\` by\n\`${artist}\` on\n\`${game}\``;
-            } else {
-                statusState = 'Status:'
-                usergame = '\`-\`'
-            }
-        }
-
-        try {
-            var lastmsg = m.lastMessage.content;
-        } catch (error) {
-            lastmsg = '`-` <a:thinkColapse:773222808057675826>';
-        }
-        if (lastmsg.length > 13) {
-            lastmsg = lastmsg.substr(0, 13) + '...';
         } else {
-            lastmsg = `${m.lastMessage.content ? m.lastMessage.content : '`-` <a:thinkColapse:773222808057675826>'}`;
-        }
-        let mobileEmoji;
-        try {
-            if (u.presence.clientStatus.mobile) {
-                mobileEmoji = '<:mobile:802732331571740742>'
-            } else {
-                mobileEmoji = ''
+            if (m.presence !== [] || null || undefined) { // Since v13 it can be any of this bs
+                // Custom status logic, checks if they have a custom status, playing a game or listening to something on spotify
+                for (let i = m.presence.activities.length - 1; i >= 0; i--) {
+                    if (m.presence.activities[i].type === 'CUSTOM') { // If status type is custom, set userstate to that
+                        statusState = 'Custom Status:'
+                        let state = m.presence.activities[i].state;
+                        let emoji = await message.guild.emojis.cache.get(m.presence.activities[i].emoji.id)
+                        if (state && emoji) {
+                            usergame = `${emoji} \`${cutTo(state, 0, 25, true)}\``
+                        } else if (state && !emoji) {
+                            usergame = `\`${cutTo(state, 0, 25, true)}\``
+                        } else if (!state && emoji) {
+                            usergame = emoji
+                        } else usergame = '\`-\`'
+                        //usergame = await cutTo(state !== null ? `\`${state}\`` : '\`-\`', 0, 20, true);
+                    } else if (m.presence.activities[i].type === 'PLAYING') { // If status type is playing, set status to game name
+                        statusState = 'Playing:'
+                        usergame = m.presence.activities[i].name;
+                        usergame = `\`${await cutTo(usergame, 0, 25, true)}\``;
+                    } else if (m.presence.activities[i].type === 'LISTENING') { // If status type is listening, show song info
+                        statusState = 'Listening to:'
+                        // Get song name
+                        let songName = m.presence.activities[i].details || 'Unknown';
+                        songName = await cutTo(songName, 0, 25, true)
+                        // Get artist name
+                        let artist = m.presence.activities[i].state || 'Unknown';
+                        artist = await cutTo(artist, 0, 25)
+                        // Get album name
+                        let album = m.presence.activities[i].assets.largeText || m.presence.activities[i].assets.smallText || 'Spotify'
+                        album = await cutTo(album, 0, 25, true)
+                        usergame = `\`${songName}\` by\n\`${artist}\` on\n\`${album}\``;
+                    }
+                }
             }
-        } catch (error) {
-            mobileEmoji = ''
+
+            let createdAt = Math.floor(this.client.users.cache.get(m.id).createdAt.getTime() / 1000)
+            let joinedAt = Math.floor(message.guild.members.cache.get(m.id).joinedAt.getTime() / 1000)
+            // Main embed with member
+            const userInfoEmbed = new MessageEmbed()
+                .setDescription(`${u.tag} ${statusIndicator}`)
+                .setColor(crimson)
+                .addField(`Joined:`, `<t:${joinedAt}:R> <a:blobDJ:773206358991962132>`, true)
+                .addField(`Registered:`, `<t:${createdAt}:R> <a:animatedCool:773205297782325259>`, true)
+                .addField(`Nickname:`, `${m.nickname ? m.nickname : '`-` <a:superRotating:773222147148742657>'}`, true)
+                .addField(`Bot:`, `\`${m.bot ? 'true' : 'false'}\` <a:dancingSquidward:773219104479379467>`, true)
+                .addField(`Highest role:`, `\`${highestRole}\` <a:handsClap:773222150676807716>`, true)
+                .addField(statusState ? statusState : 'Status:', usergame ? usergame : '\`-\`', true)
+                .setThumbnail(u.displayAvatarURL({ dynamic: true }))
+                .setFooter(`ID: ${m.id}`)
+                .setTimestamp();
+
+            await message.channel.send({ embeds: [userInfoEmbed] }).catch((err) => console.log(err));
         }
-
-
-        const userInfoEmbed = this.client.util.embed()
-            .setAuthor(u.tag + mobileEmoji, u.displayAvatarURL({ dynamic: true }))
-            .setDescription(u)
-            .setColor(crimson)
-            .addField(`Joined on:`, `\`${message.guild.members.cache.get(u.id).joinedAt.toUTCString().substr(0, 16)}\` <a:blobDJ:773206358991962132>`, true)
-            .addField(`Registered on:`, `\`${this.client.users.cache.get(u.id).createdAt.toUTCString().substr(0, 16)}\` <a:animatedCool:773205297782325259>`, true)
-            .addField(`Status:`, `\`${capitalize}\` <a:extremeShy:830640970642227211>`, true)
-            .addField(`Nickname:`, `${u.nickname ? u.nickname : '`-` <a:superRotating:773222147148742657>'}`, true)
-            .addField(`Last message`, `${m.lastMessage ? `[${lastmsg}](${m.lastMessage.url})` : '`-`'}`, true)
-            .addField(`Last channel:`, u.lastMessage ? `<#${u.lastMessageChannelId}>` : `\`-\``, true)
-            .addField(`Bot:`, `\`${u.bot ? 'True' : 'False'}\` <a:dancingSquidward:773219104479379467>`, true)
-            .addField(`Highest role:`, `\`${highestRole}\` <a:handsClap:773222150676807716>`, true)
-            .addField(statusState ? statusState : 'Activity:', usergame ? usergame : `\`-\``, true)
-            .setThumbnail(u.displayAvatarURL({ dynamic: true }))
-            .setFooter(`ID: ${u.id}`)
-            .setTimestamp();
-
-        await message.channel.send({ embeds: [userInfoEmbed] }).catch((err) => console.log(err));
     }
 }
 module.exports = Userinfo;
