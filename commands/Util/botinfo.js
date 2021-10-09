d0151054cd2d15315af2169d7902d644fa5ebf1e
BluePotatoBP <lprwasunavailable@gmail.com>
2021-10-10 00:43:58 +0200

    guess what, more animated emoji

--- commands/Util/botinfo.js
+++ commands/Util/botinfo.js
@@ -23,9 +23,7 @@ class Botinfo extends Command {
     async exec(message) {
         await delMsg(message);
 
-        let bicon = client.user.displayAvatarURL({
-            dynamic: true
-        });
+        let bicon = client.user.displayAvatarURL({ dynamic: true });
         let seconds = parseInt((client.uptime / 1000) % 60),
             minutes = parseInt((client.uptime / (1000 * 60)) % 60),
             hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
@@ -36,55 +34,21 @@ class Botinfo extends Command {
 
         const embed = new Discord.MessageEmbed()
             .setAuthor(`${client.user.username} - ${lang(message, 'command.botinfo.embed.author')}`, bicon)
-            .addField(
-                `**${lang(message, 'command.botinfo.embed.field.one')}**`,
-                `\`BluePotatoBP#5214\` <a:animatedCool:773205297782325259>`,
-                true
-            )
+            .addField(`**${lang(message, 'command.botinfo.embed.field.one')}**`, `\`BluePotatoBP#5214\``, true)
             .addField(`**${lang(message, 'command.botinfo.embed.field.two')}**`, `\`${client.user.id}\``, true)
-            .addField(
-                `**${lang(message, 'command.botinfo.embed.field.three')}**`,
-                `\`${process.env.VERSION}\` <a:gears:773203929507823617>`,
-                true
-            )
-            .addField(
-                `**${lang(message, 'command.botinfo.embed.field.six')}**`,
-                `${lang(message, 'command.botinfo.embed.field.seven')} \`${client.users.cache.size}\`
-			${lang(message, 'command.botinfo.embed.field.eight')} \`${client.guilds.cache
-                    .size}\` <a:blobSnowball1:773207107376906241><a:blobSnowball2:773207107398926357>
-			${lang(message, 'command.botinfo.embed.field.nine')} \`${client.channels.cache.size}\``,
-                true
-            )
-            .addField(
-                `**${lang(message, 'command.botinfo.embed.field.four')}**`,
-                `\`${client.user.createdAt.toUTCString().substr(0, 16)}\` <a:blobDJ:773206358991962132>`,
-                true
-            )
-            .addField(
-                `**${lang(message, 'command.botinfo.embed.field.five')}**`,
-                `\`${hours}\`h \`${minutes}\`m \`${seconds}\`s`,
-                true
-            )
-            .addField(
-                `**${lang(message, "command.botinfo.embed.field.ten")}**`,
-                `Akairo: \`v${Akairo.version}\`\nDiscordJS: \`v${Discord.version}\`\nNodeJS: \`${process.version}\``,
-                true
-            )
-            .addField(
-                `**${lang(message, "command.botinfo.embed.field.eleven")}**`,
-                `${lang(message, "command.botinfo.embed.field.thirteen")} [Invite](https://discord.gg/v8zkSc9 'Support Server')`,
-                true
-            )
+            .addField(`**${lang(message, 'command.botinfo.embed.field.three')}**`, `\`${process.env.VERSION}\``, true)
+            .addField(`**${lang(message, 'command.botinfo.embed.field.four')}**`, `\`${client.user.createdAt.toUTCString().substr(0, 16)}\``, true)
+            .addField(`**${lang(message, 'command.botinfo.embed.field.five')}**`, `\`${hours}\`h \`${minutes}\`m \`${seconds}\`s`, true)
+            .addField(`**${lang(message, "command.botinfo.embed.field.ten")}**`, `Akairo: \`v${Akairo.version}\`\nDiscordJS: \`v${Discord.version}\`\nNodeJS: \`${process.version}\``, true)
+            .addField(`**${lang(message, "command.botinfo.embed.field.eleven")}**`, `${lang(message, "command.botinfo.embed.field.thirteen")} [Invite](https://discord.gg/v8zkSc9 'Support Server')`, true)
             .addField(
                 `**${lang(message, "command.botinfo.embed.field.twelve")}**`,
-                `${lang(message, "command.botinfo.embed.field.fourteen")} [Invite](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2081291519 'Invite the bot')`,
-                true
-            )
+                `${lang(message, "command.botinfo.embed.field.fourteen")} [Invite](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=1342504007 'Invite the bot')`, true)
             .setThumbnail(bicon)
             .setTimestamp()
             .setColor(crimson);
 
-        message.channel.send({ embeds: [embed] });
+        await message.channel.send({ embeds: [embed] });
     }
 }
 module.exports = Botinfo;
