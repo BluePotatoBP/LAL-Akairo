9a892cb1e1b305cb1c8ae7ca5b85f4a54c4ae649
BluePotatoBP <lprwasunavailable@gmail.com>
2021-10-22 23:23:01 +0200

    completely rewrote say command and updated desc

--- commands/Fun/say.js
+++ commands/Fun/say.js
@@ -1,22 +1,22 @@
 const { Command } = require('discord-akairo');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Permissions } = require('discord.js');
 const owoify = require('owoify-js').default;
 const mapping = '¡"#$%⅋,)(*+\'-˙/0ƖᘔƐᔭϛ9Ɫ86:;<=>?@∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄Z[/]^_`ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz{|}~';
 const OFFSET = '!'.charCodeAt(0);
-const { darkRed, crimson } = require('../../assets/colors.json');
+const { crimson } = require('../../assets/colors.json');
 const { delMsg } = require('../../assets/tools/util');
 
 class Say extends Command {
     constructor() {
         super('say', {
-            aliases: ['say', 's'],
+            aliases: ['say', 's', 'echo'],
             category: 'Fun',
             cooldown: 5000,
             ratelimit: 2,
             ownerOnly: false,
             description: {
                 content: 'Make me say anything!',
-                usage: '<text>\n \n[-e|-embed]\n[-owo|-uwu|-uvu (intensity)]\n[-f|-flip]\n\n{ [-av|-avatar] [-gi|-guildicon] [-ts|-timestamp] [color:#hex] }\n',
+                usage: '<text>\n\n[-e|-embed]\n[-owo|-uwu|-uvu (intensity)]\n[-f|-flip]\n\n{ [-av|-avatar] [-gi|-guildicon] [-ts|-timestamp] [color:#hex] }\n',
                 syntax: '<> - necessary, [] - optional, {} - embed compatible'
             },
             args: [
@@ -33,7 +33,7 @@ class Say extends Command {
                 { // Embed flags  
                     id: 'embed',
                     match: 'flag',
-                    flag: ['-e', '-embed']
+                    flag: ['-em', '-embed']
                 },
                 {
                     id: 'avatar',
@@ -56,7 +56,7 @@ class Say extends Command {
                     flag: ['-color:', '-c:'],
                     default: crimson
                 },
-                /* {
+                {
                     id: 'channel',
                     match: 'option',
                     type: 'textChannel',
@@ -72,9 +72,9 @@ class Say extends Command {
                 {
                     id: 'reply',
                     match: 'option',
-                    type: 'guildMessage',
-                    flag: ['-reply:', '-rp:'],
-                }, */
+                    type: 'message',
+                    flag: ['-reply:', '-rp:']
+                },
                 { // Text type flags
                     id: 'uvu',
                     match: 'flag',
@@ -100,65 +100,51 @@ class Say extends Command {
     }
     async exec(message, args) {
         await delMsg(message);
+
+        let originalText = args.text;
         // Flipping function
-        let flipText = args.text.split('').map((c) => c.charCodeAt(0) - OFFSET).map((c) => mapping[c] || ' ').reverse().join('');
-        // Check if the user wants an embed
-        if (args.embed) {
-            // Staffrole check
-            let cachedGuild = staffRole.find(c => c.guild == message.guild.id)
-            if (!cachedGuild) return await message.channel.send({ content: `${lang(message, "staffroleEmbed.noneFound")} \`${process.env.PREFIX}config staffrole\`` });
-            let role = message.guild.roles.cache.get(cachedGuild.role)
-            if (!role) return await message.channel.send({ content: `${lang(message, "staffroleEmbed.noneFound")} \`${process.env.PREFIX}config staffrole\`` });
-            let memberRoles = message.member._roles;
-            // If the user has the staffrole, proceed
-            if (memberRoles.some(r => cachedGuild.role === r)) {
-                // Define embed
-                const eembed = new MessageEmbed().setColor(crimson);
-                // Check for other text flags inside the embed
-                if (args.uvu) {
-                    eembed.setDescription(owoify(args.text), 'uvu');
-                } else if (args.uwu) {
-                    eembed.setDescription(owoify(args.text), 'uwu');
-                } else if (args.owo) {
-                    eembed.setDescription(owoify(args.text), 'owo');
-                } else eembed.setDescription(args.text);
-                // Checks if the embed should have the user avatar as the thumbnail
-                if (args.avatar) eembed.setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
-                // Checks if the embed should have the guild icon as the thumbnail
-                if (args.guildicon) eembed.setThumbnail(message.guild.iconURL({ dynamic: true }));
+        let flipText = originalText.split('').map((c) => c.charCodeAt(0) - OFFSET).map((c) => mapping[c] || ' ').reverse().join('');
+        // If the user has staff role they can send whatever
+        if (message.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
+            // Check if the user wants an embed
+            if (args.embed) {
+                const sayEmbed = new MessageEmbed().setColor(crimson);
+                // Check for text flags inside the embed
+                if (args.uvu) { originalText = owoify(originalText, 'uvu'); }
+                else if (args.uwu) { originalText = owoify(originalText, 'uwu'); }
+                else if (args.owo) { originalText = owoify(originalText, 'owo'); }
+                else if (args.flip) { originalText = flipText; }
+                // Other embed options
+                if (args.timestamp) sayEmbed.setTimestamp();
+                if (args.color) sayEmbed.setColor(args.color);
+                if (args.avatar) sayEmbed.setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
+                if (args.guildicon) sayEmbed.setThumbnail(message.guild.iconURL({ dynamic: true }));
                 // Checks if both avatar and guildicon flags are in the message
                 if (args.avatar && args.guildicon) return await message.channel.send({ content: 'Pick either avatar or guild icon, smh my head my head...' });
-                // Checks if there should be a timestamp in the embed
-                if (args.timestamp) eembed.setTimestamp();
-                // Checks if it should change the embed color
-                if (args.color) eembed.setColor(args.color);
-                // Checks if it should flip the message
-                if (args.flip) eembed.setDescription(flipText);
 
-                /* if (args.edit) {
-                    await args.edit.edit({ embeds: [eembed] });
-                } else { */
-                    await args.channel.send({ embeds: [eembed] }).catch(() => { });
-                /* } */
-            } else {
-                const staffroleEmbed = new MessageEmbed()
-                    .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
-                    .setDescription(`${lang(message, "staffroleEmbed.desc1")} ${role} ${lang(message, "staffroleEmbed.desc2")}`)
-                    .setColor(darkRed)
-                    .setTimestamp()
+                sayEmbed.setDescription(originalText)
 
-                await message.channel.send({ embeds: [staffroleEmbed] }).catch(() => { });
+                // Check if it should edit the message
+                if (args.edit) { await args.edit.edit({ embeds: [sayEmbed] }).catch(() => { }); }
+                else if (args.reply) { await args.reply.reply({ embeds: [sayEmbed] }).catch(() => { }); }
+                else await args.channel.send({ embeds: [sayEmbed] }).catch(() => { });
+
+            } else {// Check what type of text the user wants to be sent
+                if (args.uvu) { originalText = owoify(originalText, 'uvu'); }
+                else if (args.uwu) { originalText = owoify(originalText, 'uwu'); }
+                else if (args.owo) { originalText = owoify(originalText, 'owo'); }
+                else if (args.flip) { originalText = flipText; }
+
+                if (args.edit) { return await args.edit.edit({ content: originalText }).catch(() => { }); }
+                else if (args.reply) { return await args.reply.reply({ content: originalText }).catch(() => { }); }
+                else return await args.channel.send({ content: originalText }).catch(() => { });
             }
-        } else {// Check what type of text the user wants to be sent
-            if (args.uvu) {
-                return await args.channel.send({ content: owoify(args.text, 'uvu') });
-            } else if (args.uwu) {
-                return await args.channel.send({ content: owoify(args.text, 'uwu') });
-            } else if (args.owo) {
-                return await args.channel.send({ content: owoify(args.text, 'owo') });
-            } else if (args.flip) {
-                return await args.channel.send({ content: flipText });
-            } else return await args.channel.send({ content: args.text });
+        } else {
+            if (args.uvu) { originalText = owoify(originalText, 'uvu'); }
+            else if (args.uwu) { originalText = owoify(originalText, 'uwu'); }
+            else if (args.owo) { originalText = owoify(originalText, 'owo'); }
+
+            return await args.channel.send({ content: originalText }).catch(() => { });
         }
     }
 }
